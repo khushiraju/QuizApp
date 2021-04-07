8a81584e4d42776da66ff0c7a46e56474d6166cf
Khushi Raju <khushisurajraju@gmail.com>
2021-04-07 11:57:41 -0500

    added code file
    code file

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // copy paste all code changes here
+    // https://docs.google.com/document/d/1io69aAmL-Ryfk2FjgMh9KWGuCvxa4gK72VA0blPfUk8/edit?usp=sharing
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
