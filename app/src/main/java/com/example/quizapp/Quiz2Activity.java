d3982dc950c56e9944bb4408d622b36104d94c77
sharmas0104 <sharmas0104@gmail.com>
2021-04-20 08:08:55 -0500

    added Back Button code to quiz activity 2 and 3

--- app/src/main/java/com/example/quizapp/Quiz2Activity.java
+++ app/src/main/java/com/example/quizapp/Quiz2Activity.java
@@ -2,7 +2,9 @@ package com.example.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Quiz2Activity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class Quiz2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.quiz2);
+    }
+
+
+    public void backButton(View view) {
+        Intent intent = new Intent(this, ChooseQuizActivity.class);
+        startActivity(intent);
     }
 }
