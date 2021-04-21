c35bbe0b4d6aa199d3e7336c84f67a91dd140eb5
sharmas0104 <sharmas0104@gmail.com>
2021-04-20 19:10:00 -0500

    signUpPage also connects to quizFeed through GO button

--- app/src/main/java/com/example/quizapp/CreateAccountActivity.java
+++ app/src/main/java/com/example/quizapp/CreateAccountActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CreateAccountActivity extends AppCompatActivity {
 
@@ -14,5 +15,14 @@ public class CreateAccountActivity extends AppCompatActivity {
 
 
         Intent intent = getIntent();
+    }
+
+
+    public void goToQuizFeed(View view) {
+
+        Intent intent = new Intent(this, ChooseQuizActivity.class);
+
+        startActivity(intent);
+
     }
 }
