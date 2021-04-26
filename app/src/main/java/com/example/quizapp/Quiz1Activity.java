c78db774e4e22ca40462485bc83f77033f36b84e
Anika Bansal <anikabansal21@gmail.com>
2021-04-26 11:49:32 -0500

    updated code in java files for quizzes

--- app/src/main/java/com/example/quizapp/Quiz1Activity.java
+++ app/src/main/java/com/example/quizapp/Quiz1Activity.java
@@ -5,25 +5,105 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class Quiz1Activity extends AppCompatActivity {
 
 
-   public static final String EXTRA_NAME = "com.example.quizapp.NAME";
-
+    public static final String FINAL_ANSWER = "com.example.quizapp.FINALSTRING";
+    ArrayList<String> extrovert = new ArrayList<String>();
+    ArrayList<String> introvert = new ArrayList<String>();
+    public String finalString = " ";
 
 
 
     @Override
 
 
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.quiz1);
 
+
+        Button q1o1, q1o2, q2o1, q2o2, q3o1, q3o2;
+        //final ConstraintLayout constraintLayout;
+
+
+
+        q1o1 = findViewById(R.id.q1option1);
+        q1o2 = findViewById(R.id.q1option2);
+        q2o1 = findViewById(R.id.q2option1);
+        q2o2 = findViewById(R.id.q2option2);
+        q3o1 = findViewById(R.id.q3option1);
+        q3o2 = findViewById(R.id.q3option2);
+
+        //constraintLayout = findViewById(R.id.color);
+
         Intent intent = getIntent();
+
+        q1o1.setOnClickListener(new View.OnClickListener() {
+            // @Override
+            public void onClick(View view) {
+                // set the color to relative layout
+                //q1o1.setBackgroundColor(Color.BLUE);
+                getAnswers(q1o1);
+
+            }
+        });
+
+        q1o2.setOnClickListener(new View.OnClickListener() {
+            // @Override
+            public void onClick(View view) {
+                // set the color to relative layout
+                //q1o1.setBackgroundColor(Color.BLUE);
+                getAnswers(q1o2);
+
+            }
+        });
+
+        q2o1.setOnClickListener(new View.OnClickListener() {
+            // @Override
+            public void onClick(View view) {
+                // set the color to relative layout
+                //q1o1.setBackgroundColor(Color.BLUE);
+                getAnswers(q2o1);
+
+            }
+        });
+
+        q2o2.setOnClickListener(new View.OnClickListener() {
+            // @Override
+            public void onClick(View view) {
+                // set the color to relative layout
+                //q1o1.setBackgroundColor(Color.BLUE);
+                getAnswers(q2o2);
+
+            }
+        });
+
+        q3o1.setOnClickListener(new View.OnClickListener() {
+            // @Override
+            public void onClick(View view) {
+                // set the color to relative layout
+                //q1o1.setBackgroundColor(Color.BLUE);
+                getAnswers(q3o1);
+
+            }
+        });
+
+        q3o2.setOnClickListener(new View.OnClickListener() {
+            // @Override
+            public void onClick(View view) {
+                // set the color to relative layout
+                //q1o1.setBackgroundColor(Color.BLUE);
+                getAnswers(q3o2);
+
+            }
+        });
+
     }
 
 
@@ -42,11 +122,40 @@ public class Quiz1Activity extends AppCompatActivity {
 
     public void submitResults(View view) {
 
+        if (extrovert.size() > introvert.size() ) {
+
+            finalString = "Extrovert";
+
+        }
+        else {
+
+            finalString = "Introvert";
+        }
+
+        extrovert.clear();
+        introvert.clear();
+
         Intent intent = new Intent (this, QuizResultsActivity.class);
+        intent.putExtra(FINAL_ANSWER, finalString);
         startActivity(intent);
     }
 
     // ROUGH SKETCH FOR WHAT THE QUIZ CODE WILL LOOK LIKE
 
+    public void getAnswers(View answer) {
 
+        String choice = answer.getResources().getResourceName(answer.getId());
+
+
+        if (choice.contains("option1")) {
+
+            extrovert.add(choice);
+        }
+
+        else {
+
+            introvert.add(choice);
+        }
+
+    }
     }
--- app/src/main/java/com/example/quizapp/Quiz3Activity.java
+++ app/src/main/java/com/example/quizapp/Quiz3Activity.java
@@ -5,16 +5,102 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
 
 public class Quiz3Activity extends AppCompatActivity {
+
+    public static final String FINAL_ANSWER = "com.example.quizapp.FINALSTRING";
+    ArrayList<String> active = new ArrayList<String>();
+    ArrayList<String> lazy = new ArrayList<String>();
+    public String finalString = " ";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.quiz3);
 
+
+        Button q1o1, q1o2, q2o1, q2o2, q3o1, q3o2;
+        //final ConstraintLayout constraintLayout;
+
+
+
+        q1o1 = findViewById(R.id.q1option1);
+        q1o2 = findViewById(R.id.q1option2);
+        q2o1 = findViewById(R.id.q2option1);
+        q2o2 = findViewById(R.id.q2option2);
+        q3o1 = findViewById(R.id.q3option1);
+        q3o2 = findViewById(R.id.q3option2);
+
+        //constraintLayout = findViewById(R.id.color);
+
         Intent intent = getIntent();
+
+        q1o1.setOnClickListener(new View.OnClickListener() {
+            // @Override
+            public void onClick(View view) {
+                // set the color to relative layout
+                //q1o1.setBackgroundColor(Color.BLUE);
+                getAnswers(q1o1);
+
+            }
+        });
+
+        q1o2.setOnClickListener(new View.OnClickListener() {
+            // @Override
+            public void onClick(View view) {
+                // set the color to relative layout
+                //q1o1.setBackgroundColor(Color.BLUE);
+                getAnswers(q1o2);
+
+            }
+        });
+
+        q2o1.setOnClickListener(new View.OnClickListener() {
+            // @Override
+            public void onClick(View view) {
+                // set the color to relative layout
+                //q1o1.setBackgroundColor(Color.BLUE);
+                getAnswers(q2o1);
+
+            }
+        });
+
+        q2o2.setOnClickListener(new View.OnClickListener() {
+            // @Override
+            public void onClick(View view) {
+                // set the color to relative layout
+                //q1o1.setBackgroundColor(Color.BLUE);
+                getAnswers(q2o2);
+
+            }
+        });
+
+        q3o1.setOnClickListener(new View.OnClickListener() {
+            // @Override
+            public void onClick(View view) {
+                // set the color to relative layout
+                //q1o1.setBackgroundColor(Color.BLUE);
+                getAnswers(q3o1);
+
+            }
+        });
+
+        q3o2.setOnClickListener(new View.OnClickListener() {
+            // @Override
+            public void onClick(View view) {
+                // set the color to relative layout
+                //q1o1.setBackgroundColor(Color.BLUE);
+                getAnswers(q3o2);
+
+            }
+        });
+
     }
+
 
     public void backButton(View view) {
         Intent intent = new Intent(this, ChooseQuizActivity.class);
@@ -23,7 +109,39 @@ public class Quiz3Activity extends AppCompatActivity {
 
     public void submitResults(View view) {
 
+        if (active.size() > lazy.size() ) {
+
+            finalString = "Active";
+
+        }
+        else {
+
+            finalString = "Lazy";
+        }
+
+        active.clear();
+        lazy.clear();
+
         Intent intent = new Intent (this, QuizResultsActivity.class);
+        intent.putExtra(FINAL_ANSWER, finalString);
         startActivity(intent);
+    }
+
+
+    public void getAnswers(View answer) {
+
+        String choice = answer.getResources().getResourceName(answer.getId());
+
+
+        if (choice.contains("option1")) {
+
+            active.add(choice);
+        }
+
+        else {
+
+            lazy.add(choice);
+        }
+
     }
 }
