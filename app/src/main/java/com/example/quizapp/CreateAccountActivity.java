5d25fe0996e4f1b4337988ec1e48b464ed611526
Anika Bansal <anikabansal21@gmail.com>
2021-04-28 12:16:10 -0500

    tried to make firebase work

--- app/src/main/java/com/example/quizapp/CreateAccountActivity.java
+++ app/src/main/java/com/example/quizapp/CreateAccountActivity.java
@@ -1,12 +1,42 @@
 package com.example.quizapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import android.content.Context;
+import android.util.Log;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class CreateAccountActivity extends AppCompatActivity {
+
+    public final String TAG = "CreateAccountActivity";
+    private String name = "";
+            //nameID.getResources().getResourceName(nameID.getId());
+    private String email = "";
+    private String password = "";
+
+    // Constants to use for labels in database
+    public static final String NAME_KEY = "name";
+    public static final String EMAIL_KEY = "email";
+    public static final String PASSWORD_KEY = "password";
+
+
+    // reference to entire database
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +50,67 @@ public class CreateAccountActivity extends AppCompatActivity {
 
     public void goToQuizFeed(View view) {
 
+        addEvent();
+
         Intent intent = new Intent(this, ChooseQuizActivity.class);
 
         startActivity(intent);
 
+    }
+
+    public void addEvent() {
+        // Creates a key-value map of the object to add to the collection
+        Map<String, Object> user = new HashMap<String, Object>();
+        // Adds the all the key-value pairs to this object
+        user.put(NAME_KEY, name);
+        user.put(EMAIL_KEY, email);
+        user.put(PASSWORD_KEY, password);
+        Log.i(TAG, user.toString());
+
+        db.collection("events")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        toastMessage("Event stored successfully");
+                        Log.i(TAG, "Success");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        toastMessage("Event failed to add");
+                        Log.i(TAG, "Failure");
+                    }
+                });
+
+        // Clear the event name field
+        EditText eventNameET = (EditText) findViewById(R.id.nameID);
+        eventNameET.setText("");
+
+        closeKeyboard();
+    }
+
+
+    /**
+     * This method will be called to minimize the on screen keyboard in the Activity
+     * When we get the current view, it is the view that has focus, which is the keyboard
+     * Credit - Found and suggested by Ram Dixit, 2019
+     *
+     * Source:  https://www.youtube.com/watch?v=CW5Xekqfx3I
+     */
+    private void closeKeyboard() {
+        View view = this.getCurrentFocus();     // view will refer to the keyboard
+        if (view != null ){                     // if there is a view that has focus
+            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+
+    /*
+        This method improves readability of the code for toast messages.  It is a simple helper method
+     */
+    private void toastMessage(String message){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
