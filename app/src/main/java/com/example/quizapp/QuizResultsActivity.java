7f2649a21fd6795b19e7ec0784af13d3b8517af1
sharmas0104 <sharmas0104@gmail.com>
2021-05-06 12:14:12 -0500

    use some form of this code

--- app/src/main/java/com/example/quizapp/QuizResultsActivity.java
+++ app/src/main/java/com/example/quizapp/QuizResultsActivity.java
@@ -35,4 +35,50 @@ public class QuizResultsActivity extends AppCompatActivity {
         Intent intent = new Intent(this, ChooseQuizActivity.class);
         startActivity(intent);
     }
+
+    /*
+    public void addEvent() {
+        EditText name = (EditText) findViewById(R.id.nameID);
+        EditText email = (EditText) findViewById(R.id.emailID);
+        EditText password = (EditText) findViewById(R.id.passwordID);
+
+        String userName = name.getText().toString();
+        String userEmail = email.getText().toString();
+        String userPassword = password.getText().toString();
+
+        // Creates a key-value map of the object to add to the collection
+        Map<String, Object> user = new HashMap<String, Object>();
+        // Adds the all the key-value pairs to this object
+        user.put(NAME_KEY, userName);
+        user.put(EMAIL_KEY, userEmail);
+        user.put(PASSWORD_KEY, userPassword);
+        Log.i(TAG, user.toString());
+
+        db.collection("quizApp")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        toastMessage("Event stored successfully");
+                        Log.i(TAG, "Success");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        toastMessage("Event failed to add");
+                        Log.i(TAG, "Failure");
+                    }
+                });
+
+        // Clear the event name field
+        name.setText("");
+        email.setText("");
+        password.setText("");
+
+        closeKeyboard();
+    }
+
+
+     */
 }
