357234a9a78864a2e5fc2ad45bc63a3629ef3555
sharmas0104 <sharmas0104@gmail.com>
2021-05-06 12:08:08 -0500

    added signIn method to signIN activity
    aioh

--- app/src/main/java/com/example/quizapp/CreateAccountActivity.java
+++ app/src/main/java/com/example/quizapp/CreateAccountActivity.java
@@ -94,6 +94,29 @@ public class CreateAccountActivity extends AppCompatActivity {
         // [END create_user_with_email]
     }
 
+    private void signIn(String email, String password) {
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(CreateAccountActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+        // [END sign_in_with_email]
+    }
+
 
 
 
@@ -110,6 +133,7 @@ public class CreateAccountActivity extends AppCompatActivity {
 
 
         createAccount(userEmail, userPassword);
+        signIn(userEmail, userPassword);
 
         Intent intent = new Intent(this, ChooseQuizActivity.class);
 
--- app/src/main/java/com/example/quizapp/SignInActivity.java
+++ app/src/main/java/com/example/quizapp/SignInActivity.java
@@ -7,14 +7,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class SignInActivity extends AppCompatActivity {
+
+
+
+    public final String TAG = "SignInActivity";
+
+    private FirebaseFirestore db;
+    private FirebaseAuth mAuth;
 
 
     @Override
@@ -22,7 +32,7 @@ public class SignInActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_in);
 
-
+        mAuth = FirebaseAuth.getInstance();
         Intent intent = getIntent();
 
     }
@@ -30,9 +40,49 @@ public class SignInActivity extends AppCompatActivity {
 
     public void goToQuizFeed(View view) {
 
+        EditText email = (EditText) findViewById(R.id.email);
+        EditText password = (EditText) findViewById(R.id.password);
+
+
+        String userEmail = email.getText().toString();
+        String userPassword = password.getText().toString();
+
         Intent intent = new Intent(this, ChooseQuizActivity.class);
+        signIn(userEmail, userPassword);
+
 
         startActivity(intent);
+
+    }
+
+
+
+    private void signIn(String email, String password) {
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(SignInActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+        // [END sign_in_with_email]
+    }
+
+    private void reload() { }
+
+    private void updateUI(FirebaseUser user) {
 
     }
 
